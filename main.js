8dd4cd69d1c727419624ac9276a36d5b2c4bf031
Rafcyk <rafcy9@gmail.com>
2020-02-06 13:26:16 +0100

    add multi functions

--- main.js
+++ main.js
@@ -1,105 +1,201 @@
-const btnMulti = document.querySelector('.wrapMain .multi button.twoPeople');
+const btnMulti = document.querySelector(".wrapMain .multi button.twoPeople");
 
-
-const chars = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'f', 'g', 'h', 'i', 'j' , 'k', 'l','ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'r', 's', 'ś', 't', 'u', 'w', 'x', 'y', 'z', 'ź' , 'ż'];
+let lettersToEnd = 0;
+let faultCount = 0;
+const faultsNumber = 5;
+const chars = [
+  "a",
+  "ą",
+  "b",
+  "c",
+  "ć",
+  "d",
+  "e",
+  "ę",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "ł",
+  "m",
+  "n",
+  "ń",
+  "o",
+  "ó",
+  "p",
+  "r",
+  "s",
+  "ś",
+  "t",
+  "u",
+  "w",
+  "x",
+  "y",
+  "z",
+  "ź",
+  "ż"
+];
 
 let wordArray = [];
+let spanLetters = [];
+let spanChars = [];
 
+const makeGame = () => {
+  lettersToEnd = 0;
 
-const makeGame = ()=>{
-    const divGame = document.createElement('div');
-    divGame.className = 'game';
+  const divGame = document.createElement("div");
+  divGame.className = "game";
 
-    document.body.appendChild(divGame);
+  document.body.appendChild(divGame);
 
-    const divRules = document.createElement('div');
-    divRules.className = 'rules';
+  const imgExit = document.createElement("img");
+  imgExit.src = "exit.png";
+  divGame.appendChild(imgExit);
 
-    divGame.appendChild(divRules);
+  const divRules = document.createElement("div");
+  divRules.className = "rules";
 
-    const pTextRules = document.createElement('p');
-    pTextRules.className = 'textRules';
-    pTextRules.textContent = 'Witaj w grze wisielec dla dwóch osób. Gra polega na tym, że jedna osoba wpisuje hasło do odgadnięcia przez drugą osobę, po czym klikajac wybrane litery odgaduje hasło. Gra kończy się gdy jedna osoba odgadnie hasło lub wisielec uzupełni się do końca.';
-    
-    const btnStartGame = document.createElement('button');
-    btnStartGame.className = 'startGame';
-    btnStartGame.textContent = 'Dalej';
+  divGame.appendChild(divRules);
 
-    divRules.appendChild(pTextRules);
-    divRules.appendChild(btnStartGame);
+  const pTextRules = document.createElement("p");
+  pTextRules.className = "textRules";
+  pTextRules.textContent =
+    "Witaj w grze wisielec dla dwóch osób. Gra polega na tym, że jedna osoba wpisuje hasło do odgadnięcia przez drugą osobę, po czym klikajac wybrane litery odgaduje hasło. Gra kończy się gdy jedna osoba odgadnie hasło lub wisielec uzupełni się do końca.";
 
-    const playGame =()=>{
-        while(divGame.firstChild) divGame.removeChild(divGame.firstChild)
+  const btnStartGame = document.createElement("button");
+  btnStartGame.className = "startGame";
+  btnStartGame.textContent = "Dalej";
 
-        const divAddWord = document.createElement('div');
-        divAddWord.className = 'addWord';
+  divRules.appendChild(pTextRules);
+  divRules.appendChild(btnStartGame);
 
-        divGame.appendChild(divAddWord);
+  const playGame = () => {
+    lettersToEnd = 0;
+    spanLetters = [];
+    while (divGame.firstChild) divGame.removeChild(divGame.firstChild);
 
-        const inputWord = document.createElement('input');
-        inputWord.placeholder = 'Wpisz hasło';
+    divGame.appendChild(imgExit);
 
-        const btnPlay = document.createElement('button');
-        btnPlay.className = 'play';
-        btnPlay.textContent = 'Graj';
+    const divAddWord = document.createElement("div");
+    divAddWord.className = "addWord";
 
+    divGame.appendChild(divAddWord);
 
-        divAddWord.appendChild(inputWord);
-        divAddWord.appendChild(btnPlay);
+    const inputWord = document.createElement("input");
+    inputWord.placeholder = "Wpisz hasło";
 
-        const addWord = ()=> {
-            while(divGame.firstChild) divGame.removeChild(divGame.firstChild)
+    const btnPlay = document.createElement("button");
+    btnPlay.className = "play";
+    btnPlay.textContent = "Graj";
 
-                const divWrapWord = document.createElement('div');
-                divWrapWord.className = 'wrapWord';
-                divGame.appendChild(divWrapWord);
+    divAddWord.appendChild(inputWord);
+    divAddWord.appendChild(btnPlay);
 
-                const word = inputWord.value;
-                wordArray = [...word];
+    const addWord = () => {
+      if (inputWord.value) {
+        while (divGame.firstChild) divGame.removeChild(divGame.firstChild);
 
-                for(let i = 0; i < wordArray.length; i ++){
-                    const divWrapLetter = document.createElement('div');
-                    divWrapLetter.className = 'wrapLetter';
+        divGame.appendChild(imgExit);
 
-                    divWrapWord.appendChild(divWrapLetter);
+        const divWrapWord = document.createElement("div");
+        divWrapWord.className = "wrapWord";
+        divGame.appendChild(divWrapWord);
 
-                    const spanLetter = document.createElement('span');
-                    spanLetter.className = 'letter';
-                    spanLetter.textContent = wordArray[i];
+        const word = inputWord.value.toLowerCase();
+        wordArray = [...word];
 
-                    const spanLine = document.createElement('span');
-                    spanLine.className = 'line';
+        for (let i = 0; i < wordArray.length; i++) {
+          if (wordArray[i] !== " ") {
+            const divWrapLetter = document.createElement("div");
+            divWrapLetter.className = "wrapLetter";
 
-                    divWrapLetter.appendChild(spanLetter);
-                    divWrapLetter.appendChild(spanLine);
-                }
-            
+            divWrapWord.appendChild(divWrapLetter);
 
-            for(let i = 0; i < chars.length; i++){
-                
+            const spanLetter = document.createElement("span");
+            spanLetter.className = "letter";
+            spanLetter.textContent = wordArray[i];
+            spanLetter.style.opacity = 0;
+            spanLetters.push(spanLetter);
 
+            const spanLine = document.createElement("span");
+            spanLine.className = "line";
 
-            }
+            divWrapLetter.appendChild(spanLetter);
+            divWrapLetter.appendChild(spanLine);
+          } else if (wordArray[i] == " ") {
+            const divWrapLetter = document.createElement("div");
+            divWrapLetter.className = "wrapLetter";
 
-            
+            divWrapWord.appendChild(divWrapLetter);
 
+            const spanLetter = document.createElement("span");
+            spanLetter.className = "letter";
+            spanLetter.textContent = wordArray[i];
+            spanLetter.style.opacity = 0;
 
+            divWrapLetter.appendChild(spanLetter);
+            // divWrapLetter.appendChild(spanLine);
+          }
         }
 
-        btnPlay.addEventListener('click', addWord);
+        const divWrapChars = document.createElement("div");
+        divWrapChars.className = "wrapChars";
+        divGame.appendChild(divWrapChars);
 
+        for (let i = 0; i < chars.length; i++) {
+          const divWrapChar = document.createElement("div");
+          divWrapChar.className = "wrapChar";
+          divWrapChar.textContent = chars[i];
+          divWrapChars.appendChild(divWrapChar);
 
-    }
+          // const spanChar = document.createElement('span');
+          // spanChar.className = 'char';
+          // spanChar.textContent = chars[i];
+          // divWrapChar.appendChild(spanChar);
 
+          spanChars.push(divWrapChar);
+        }
 
-    btnStartGame.addEventListener('click', playGame);
+        spanChars.forEach(char => {
+          char.addEventListener("click", () => {
+            char.style.pointerEvents = "none";
+            char.style.opacity = 0.3;
 
-}
+            for (let i = 0; i < spanLetters.length; i++) {
+              if (spanLetters[i].textContent === char.textContent) {
+                spanLetters[i].style.opacity = 1;
+                console.log(lettersToEnd);
 
+                lettersToEnd++;
+              }
+            }
+            if (lettersToEnd == spanLetters.length) {
+              console.log("wygrałes");
+              const divWin = document.createElement("div");
+              divWin.className = "winScreen";
+              divWin.textContent = "Wygrałeś!";
+              divGame.appendChild(divWin);
+              divWin.appendChild(btnStartGame);
 
+              for (let i = 0; i < spanChars.length; i++) {
+                spanChars[i].style.pointerEvents = "none";
+              }
+            }
+          });
+        });
+      } else alert("Wpisz hasło!");
+    };
 
+    btnPlay.addEventListener("click", addWord);
+  };
 
+  btnStartGame.addEventListener("click", playGame);
+  imgExit.addEventListener("click", () => {
+    document.body.removeChild(divGame);
+  });
+};
 
-
-
-btnMulti.addEventListener('click', makeGame);
+btnMulti.addEventListener("click", makeGame);
